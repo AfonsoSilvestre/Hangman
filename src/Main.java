9039f8ac4259d9365d9f5e5ba912dacb09afe41b
AfonsoSilvestre <asilvestrecontact@gmail.com>
2025-01-31 01:16:53 +0000

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new Window(); //initializes our application
+        new Window(); // initializes our application
     }
 }
